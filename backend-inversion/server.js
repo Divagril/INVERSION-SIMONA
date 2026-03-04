ed3baf5603c7c33c5f3cb3d98b2cb55bdf47d309
Flor <cordovacruzfloresmeralda@example.com>
2026-03-03 21:09:09 -0500

    Solucion final

--- backend-inversion/server.js
+++ backend-inversion/server.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const cors = require('cors');
 app.use(cors({
   origin: "*", // Permite conexiones desde cualquier origen
   methods: ["GET", "POST", "PUT", "DELETE"],
